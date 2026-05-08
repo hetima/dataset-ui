6555c18c8913da228341e0eacaad286b18a7ab81
hetima <33700+hetima@users.noreply.github.com>
2026-05-08 13:04:10 +0900

    music/index.py

---
+++ music/index.py
@@ -0,0 +1,24 @@
+from nicegui import ui
+
+
+from common.worker import Worker
+from music.app_ctx import MusicCtx
+from music.tab_main import tab_main
+from music.tab_setting import tab_setting
+
+def main_page(worker: Worker):
+    ctx = MusicCtx(worker)
+    
+    ui.markdown("""## dataset-ui-music
+ACE-Step 向けのメタデータを書き出す webui です""")
+
+    with ui.tabs().classes("w-full").classes("text-dark").props(
+        'inline-label align="left"'
+    ) as tabs:
+        main_Tab = ui.tab("main", label="メイン", icon="music_note")
+        setting_tab = ui.tab("setting", label="設定", icon="settings")
+    with ui.tab_panels(tabs, animated=False, value=main_Tab).classes("w-full"):
+        with ui.tab_panel(main_Tab):
+            tab_main(ctx)
+        with ui.tab_panel(setting_tab):
+            tab_setting(ctx)
--- music_app.py
+++ music_app.py
@@ -3,22 +3,14 @@ from pathlib import Path
 from typing import cast
 
 from nicegui import ui, app
-from nicegui.elements.table import Table
 from music.setting import cnfg
-from music.musicfile import MusicFile
 from common.worker import Worker
-from music.app_ctx import MusicCtx
-from music.tab_main import tab_main
-from music.tab_setting import tab_setting
-
+from music.index import main_page as main_page_music
 
 _worker: Worker = Worker()
 
-@ui.page("/")
-def main_page():
-    ctx = MusicCtx(_worker)
+def header():
     ui.query('body').style('font-family: Roboto, "BIZ UDPGothic", "BIZ UDPゴシック", sans-serif;')
-
     ui.add_css('''
 .infotxt {
     color: #666;
@@ -47,44 +39,50 @@ def main_page():
 }
 ''')
     ui.colors(secondary='#747474')
-    ui.markdown("""## dataset-ui-music
-ACE-Step 向けのメタデータを書き出す webui です""")
 
-    with ui.tabs().classes('w-full').classes("text-dark").props('inline-label align="left"') as tabs:
-        main_Tab = ui.tab('main',label="メイン", icon="music_note")
-        setting_tab = ui.tab('setting',label="設定", icon="settings")
-    with ui.tab_panels(tabs, animated=False, value=main_Tab).classes('w-full'):
-        with ui.tab_panel(main_Tab):
-            tab_main(ctx)
-        with ui.tab_panel(setting_tab):
-            tab_setting(ctx)
-
+def footer():
     # ═══════════════════════════════════════════════════════════════════════════════
     # 処理中UI
     # ═══════════════════════════════════════════════════════════════════════════════
-    with ui.footer(bordered=True).bind_visibility_from(ctx.worker, "is_running").style('background-color: #f2f2f2'):
+    with ui.footer(bordered=True).bind_visibility_from(_worker, "is_running").style(
+        "background-color: #f2f2f2"
+    ):
         with ui.row().classes("items-center gap-4"):
             ui.spinner(size='lg')
             with ui.column():
                 with ui.row().classes("items-center gap-4"):
                     ui.label("バックグラウンド処理を実行しています").style('color: #010101')
-                    ui.label("").style('color: #010101').bind_text_from(ctx.worker, "elapsed_time")
+                    ui.label("").style("color: #010101").bind_text_from(
+                        _worker, "elapsed_time"
+                    )
                 with ui.row().classes("items-center gap-4"):
-                    ui.button("処理を中止する", on_click=ctx.worker.request_cancel).bind_visibility_from(
-                        ctx.worker, "is_running"
-                    ).props('color="orange"').tooltip("現在の処理が済んだら終了")
-                    ui.button("処理を強制終了する", icon='warning', on_click=ctx.worker.terminate_now).bind_visibility_from(
-                        ctx.worker, "is_running"
-                    ).props('color="red"').tooltip("強制的に子プロセスを終了")
-                    ui.label().style('color: #010101').bind_text_from(ctx.worker, "status")
+                    ui.button(
+                        "処理を強制終了する",
+                        icon="warning",
+                        on_click=_worker.terminate_now,
+                    ).bind_visibility_from(_worker, "is_running").props(
+                        'color="red"'
+                    ).tooltip(
+                        "強制的に子プロセスを終了"
+                    )
+                    ui.label().style("color: #010101").bind_text_from(_worker, "status")
         ui.linear_progress(show_value=False).props("instant-feedback").bind_value_from(
-            ctx.worker, "progress"
-        ).bind_visibility_from(ctx.worker, "is_running")
+            _worker, "progress"
+        ).bind_visibility_from(_worker, "is_running")
+
+
+@ui.page("/")
+def main_page():
+    header()
+    main_page_music(_worker)
+    footer()
 
 
 @ui.page("/test")
 def test_page():
+    header()
     ui.label("test")
+    footer()
 
 
 def make_startup_message(host: str, port: int):
